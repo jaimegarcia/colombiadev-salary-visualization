c0046e08fc335aa30d6156cb45a5c96a33a91f4a
Jaime Garcia <jaimegarcia@ingeniosolido.com>
2020-07-31 23:28:07 -0500

    Updates

---
+++ precache-manifest.2b1bd3b1577aa7f8c9fefa055aa477ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4137b7c59ad059041734c930c38657",
+    "url": "/colombiadev-salary-visualization/index.html"
+  },
+  {
+    "revision": "9846d76f09a759a11842",
+    "url": "/colombiadev-salary-visualization/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c496349e06a9b50a1661",
+    "url": "/colombiadev-salary-visualization/static/js/2.adc64d0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colombiadev-salary-visualization/static/js/2.adc64d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9846d76f09a759a11842",
+    "url": "/colombiadev-salary-visualization/static/js/main.d465b76d.chunk.js"
+  },
+  {
+    "revision": "45dac2183468bfbb04c1",
+    "url": "/colombiadev-salary-visualization/static/js/runtime-main.ae351cf8.js"
+  },
+  {
+    "revision": "b3e58a6f381c1666f4180ac5746052e2",
+    "url": "/colombiadev-salary-visualization/static/media/salaries.b3e58a6f.csv"
+  }
+]);
